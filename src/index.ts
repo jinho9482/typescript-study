b83503aedc6feba61afa7e09751f6b4bef5cd468
unknown <jinhojo9482@gmail.com>
2024-03-20 20:47:18 +0900

    See how enum works and check what makes an error

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,28 @@
-// Tuple : Fixed length array with particular type of elements (good enough to use up to 2 types)
+// Enum : group of constants
 
-let array = [1, "horse"];
-array[3] = 1; // An element of an array can be added by assigning value using index
-console.log(array); // [ 1, 'horse', <1 empty item>, 1 ]
+enum Size {
+  small,
+  medium,
+  large,
+}
+console.log(Size.small); // 0
+console.log(Size.medium); // 1 (0+1)
+console.log(Size.large); // 2 (0+1+1)
 
-let array1: [number, string] = [1, "horse"];
-array1[2] = 1; // error, not assignable
-console.log(array1);
+enum Size1 {
+  small = "s",
+  medium = 1,
+  large,
+}
+console.log(Size1.small); // s
+console.log(Size1.medium); // 1
+console.log(Size1.large); // 2
 
-let array2: [number, string] = [1, "horse"];
-array2.push(2); // array2 is tuple, but its length can be changed through push method.
-console.log(array2); // [ 1, 'horse', 2 ]
-
-let array3: [number, string, undefined] = [1, "horse", undefined];
-console.log(array3); // [ 1, 'horse', undefined ]
+enum Size2 {
+  small = "s",
+  medium = 2,
+  large = [1, 2, 3],
+}
+console.log(Size1.small);
+console.log(Size1.medium);
+console.log(Size1.large); // error, enum can take only string and number type
