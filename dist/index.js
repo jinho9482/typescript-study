4aaa97f2e660a907c1a880c4ab9fda8162e09f12
unknown <jinhojo9482@gmail.com>
2024-03-20 20:53:24 +0900

    See how js file looks after compiling enum type

---
+++ dist/index.js
@@ -0,0 +1,10 @@
+"use strict";
+var Size;
+(function (Size) {
+    Size[Size["small"] = 0] = "small";
+    Size[Size["medium"] = 1] = "medium";
+    Size[Size["large"] = 2] = "large";
+})(Size || (Size = {}));
+let mySize = Size.medium;
+console.log(mySize);
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -5,24 +5,6 @@ enum Size {
   medium,
   large,
 }
-console.log(Size.small); // 0
-console.log(Size.medium); // 1 (0+1)
-console.log(Size.large); // 2 (0+1+1)
 
-enum Size1 {
-  small = "s",
-  medium = 1,
-  large,
-}
-console.log(Size1.small); // s
-console.log(Size1.medium); // 1
-console.log(Size1.large); // 2
-
-enum Size2 {
-  small = "s",
-  medium = 2,
-  large = [1, 2, 3],
-}
-console.log(Size1.small);
-console.log(Size1.medium);
-console.log(Size1.large); // error, enum can take only string and number type
+let mySize: Size = Size.medium;
+console.log(mySize);
