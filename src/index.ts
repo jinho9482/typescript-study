d058fe38eb69ef22d7551f10305812a5af0f0caf
jinho9482 <jinhojo9482@gmail.com>
2024-03-21 22:46:28 +0900

    See how type aliases work

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
-let employee: {
+type Employee = {
   readonly id: number;
   name: string;
   retire: (date: Date) => void;
-} = {
+};
+
+let employee: Employee = ({} = {
   id: 1,
   name: "Jinho",
   retire: (date: Date) => {
     console.log(date);
   },
-};
+});
