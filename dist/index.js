a24963af5faf055bfa66ce35cccc47cb15318d3c
jinho9482 <jinhojo9482@gmail.com>
2024-03-21 22:38:59 +0900

    See how object works

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,9 @@
 "use strict";
-function calcaulateTax(income) { }
+let employee = {
+    id: 1,
+    name: "Jinho",
+    retire: (date) => {
+        console.log(date);
+    },
+};
 //# sourceMappingURL=index.js.map
